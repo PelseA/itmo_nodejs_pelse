0ef072475e7a99e48b9a9dd8e75da02d66ea08a8
Aleksandra Pelse <shashka_aya@mail.ru>
2022-11-01 16:02:25 +0300

    add class for display text input

--- vue_DOM_subscriber/index.js
+++ vue_DOM_subscriber/index.js
@@ -1,25 +1,35 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        name: '',
-        lastname: ''
-    },
+  el: '#app',
+  data: {
+    name: '',
+    lastname: ''
+  },
 })
 
-const inputName = document.getElementById('nameInp');
-const inputSurname = document.getElementById('lastnameInp');
-const fullNameStr = document.getElementById('fullNameStr');
-let stringFullName = 'Full name: ';
-inputName.addEventListener('input', function(e) {
-    //todo возвращение к прежнему инпуту
-    // const regFullStr = /Full\sname:\s/i;
-    fullNameStr.innerText = stringFullName + this.value;
-});
-inputName.addEventListener('change', function(e) {
-    stringFullName = stringFullName + this.value + ' ';
-    fullNameStr.innerText = stringFullName;
-});
-inputSurname.addEventListener('input', function (e) {
-    fullNameStr.innerText = stringFullName + this.value;
-});
+// 1) пример отображения текста на странице, введенного в инпут, на Vue.js
+// реализовано с помощью директивы v-model , добавленной в разметку html
+// 2) пример отображения текста на странице, введенного в инпут, на нативном JS
+/** чтобы передать метод экземпляра класса как callback при добавлении addEventListener,
+нужно использовать привязку контекста. Иначе this будет ссылаться на событие, а не на свойства класса **/
+class displayTextInput {
+  constructor(elementInputId, elementForDisplayTextId) {
+    this.elementInput = document.getElementById(elementInputId);
+    this.elementForDisplayText = document.getElementById(elementForDisplayTextId);
+  }
 
+  passTextFromInputToElement() {
+    this.elementForDisplayText.innerText = this.elementInput.value;
+  }
+
+  passTextFromInputToElementBindContext = this.passTextFromInputToElement.bind(this);
+
+  addListenerInput() {
+    this.elementInput.addEventListener('input', this.passTextFromInputToElementBindContext);
+  }
+  addListenerInputBindContext = this.addListenerInput.bind(this);
+}
+
+const displayName = new displayTextInput('nameInp', 'nameStr');
+const displayLastname = new displayTextInput('lastnameInp', 'lastnameStr');
+displayName.addListenerInputBindContext();
+displayLastname.addListenerInputBindContext();
