237ad115d73f7a81b52c624313e475f98e86044a
Aleksandra Pelse <shashka_aya@mail.ru>
2022-12-12 00:28:36 +0300

    add tailwind

---
+++ invoice/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/**/*.{html,js,ts,jsx,tsx}",
+      "index.html"
+  ],
+  theme: {
+    extend: {
+    },
+    container: {
+      center: true,
+    },
+
+  },
+  plugins: [],
+}
